64bfda0f852f43e0b306aafe67e9d1bf7eb006af
MouseMs <mousems.kuo@gmail.com>
2014-10-09 00:53:09 +0800

    fix check bug of regex

--- ntust_hw/security/hw1.php
+++ ntust_hw/security/hw1.php
@@ -5,9 +5,9 @@ echo Go($_GET['plain'] , $_GET['key']);
 
 function Go($plain , $key){
 
-	if (preg_match("/[A-Z]+/", $plain)!=1) {
+	if (preg_match("/^[A-Z]+$/", $plain)!=1) {
 		return "Error.";
-	}elseif(preg_match("/[\d]+/", $key)!=1) {
+	}elseif(preg_match("^/[\d]+$/", $key)!=1) {
 		return "Error.";
 	}
 
