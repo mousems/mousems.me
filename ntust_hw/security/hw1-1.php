dd6dab7e4b0b2660463a77bc4709e54847a9edac
MouseMs <mousems.kuo@gmail.com>
2014-10-10 00:06:31 +0800

    lower case input

--- ntust_hw/security/hw1-1.php
+++ ntust_hw/security/hw1-1.php
@@ -5,7 +5,7 @@ echo Go($_GET['plain'] , $_GET['key']);
 
 function Go($plain , $key){
 
-	if (preg_match("/^[A-Z]+$/", $plain)!=1) {
+	if (preg_match("/^[a-z]+$/", $plain)!=1) {
 		return "Error.";
 	}elseif(preg_match("/^[\d]+$/", $key)!=1) {
 		return "Error.";
@@ -13,13 +13,13 @@ function Go($plain , $key){
 
 	echo "Input:".$plain."<br />";
 
-	preg_match_all("/([A-Z])/", $plain, $matches);
+	preg_match_all("/([a-z])/", $plain, $matches);
 	// make $plain break into piceses
 
 	echo "Output:";
 	foreach ($matches[1] as $key1 => $value) {
 		$tmp = ord($value)+$key;
-		if ($tmp>ord("Z")) {
+		if ($tmp>ord("z")) {
 			$tmp = $tmp - 26;
 		}
 		$tmp = chr($tmp);
